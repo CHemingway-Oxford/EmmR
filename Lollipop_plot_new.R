f89dbe0f0bc1aec718d56716f7028af67d386de3
CHemingway-Oxford <122449820+CHemingway-Oxford@users.noreply.github.com>
2023-04-16 22:06:42 +0200

    Christophers changes to lollipop plot
    - changed grouping and arranging by index
    - grouped by medium
    
    - plot mean of the distribution of ruptured points did not work yet.

--- Lollipop_plot_new.R
+++ Lollipop_plot_new.R
@@ -9,31 +9,45 @@ library(grid)
 library(cowplot)
 
 #Load and clean Data 
-DatenEmma <- read_excel("Doktorarbeit II/Tabellen/Medien_Vgl_Dicken,Gewichte&Pressure.xlsx", 
-                                                sheet = "max Pressure", col_types = c("skip", 
-                                                                                      "text", "skip", "numeric", "skip", 
-                                                                                      "text", "skip"), na ="NA" )
+DatenEmma <- read_excel("Desktop/Medien_Vgl_Dicken-Gewichte_Pressure.xlsx", 
+                       sheet = "max Pressure", col_types = c("skip", 
+                                                             "numeric", "skip", "numeric", "skip", 
+                                                             "numeric", "skip"))
 
-#View(DatenEmma) 
 
+
+# View(DatenEmma)
+# are these the correct colors?
 colour_palette <- c('#66c2a5' , '#fc8d62' , '#8da0cb' , '#e78ac3')
 
 data_lollipop <- DatenEmma %>% 
   select(Pressure_max_mmHg, Rupture, Medium) %>% 
   drop_na(Pressure_max_mmHg) %>%
-  mutate(ID = c(1:24))
+  # mutate(ID = c(1:24))
+  mutate(Medium=factor(Medium, levels=c('333','454510','5050'))) %>% 
+  mutate(Rupture=factor(Rupture, levels= (c("0","1"))))
+data_lollippop_mean <- data_lollipop %>% group_by(Medium, Rupture) %>% 
+  summarize(mean_preassure = mean(Pressure_max_mmHg)) %>% 
+  mutate(Medium=factor(Medium, levels=c('1','2','3'))) %>% 
+  mutate(Medium=as.numeric(Medium))
+
+
+# data_lollipop <-  droplevels(data_lollipop[!data_lollipop$Medium == '5050',])
 
 #create horizontal lollipop Plot
 p_lolli <- data_lollipop %>%
-  arrange(Pressure_max_mmHg) %>%    # First sort by val. This sort the dataframe but NOT the factor levels
-  mutate(ID=factor(ID, levels=ID)) %>%# This trick updates the factor levels
-  
-  ggplot( aes(x=ID, y=Pressure_max_mmHg)) +
+  ggplot( aes(x=Medium, y=Pressure_max_mmHg)) +
   #geom_segment(aes(xend=ID, yend=400) , size  = 0.6) +
-  geom_point(aes(shape = Rupture , color = Medium) ,  size=3 , stroke = 1) +
+  geom_jitter(aes(shape = Rupture , color = Medium) ,  size=3 , stroke = 1, width = 0.3
+              
+              ) +
+  stat_summary(data = data_lollippop_mean, fun = mean, geom = "errorbarh", 
+               aes(y = Medium, xmin = mean_preassure, xmax = mean_preassure), 
+               color = "red", size = 1.5)+
+  # geom_hline(data = data_lollippop_mean, )
   scale_shape_manual(values = c(20, 4) , labels  = c("No" , "Yes")) +
-  scale_color_manual(values = colour_palette , labels=c("45|45|10" , "3|3|3" , "50|50" , "C_100"))+
-  scale_y_continuous(limits = c(600,1415) , breaks=c(600 , 800 , 1000,1200) ,
+  scale_color_manual(values = colour_palette , labels=c("3|3|3" , "45|45|10" ))+
+  scale_y_continuous(limits = c(750,1500) , breaks=c(600 , 800 , 1000,1200, 1400) ,
                      expand = c(0,0))+
   scale_x_discrete(breaks = NULL)+
   coord_flip()+
@@ -53,8 +67,8 @@ p_lolli <- data_lollipop %>%
   xlab("")+
   ggtitle(label = "Maximum Withstood Pressure")
 
-ggsave(path = "C:/Users/Asus/Documents/Doktorarbeit II/R_Coding/Github/EmmR", filename = "Max_WithstoodPressure_MediumComp.png", plot = p_lolli, device = "png" , dpi = 1000 , 
-       units = "cm" , width = 16.8 , height = 12)
+# ggsave(path = "C/Users/ChrisHemingway/Desktop", filename = "Max_WithstoodPressure_MediumComp.png", plot = p_lolli, device = "png" , dpi = 300 ,
+#        units = "cm" , width = 16.8 , height = 12)
 
 
 #####generate common figure legend with uniform labelling:
@@ -65,3 +79,7 @@ legend <- get_legend(
     ) +
     labs(color = "Test series" , shape = "Ruptured in test"))
 
+ggsave(path = "C/Users/ChrisHemingway/Desktop", filename = "Max_WithstoodPressure_MediumComp.png", plot = p_lolli, device = "png" , dpi = 300 ,
+       units = "cm" , width = 16.8 , height = 12)
+
+p_lolli
