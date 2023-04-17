5107d3a3161810f2ff797b43f49fbb4a076af426
CHemingway-Oxford <122449820+CHemingway-Oxford@users.noreply.github.com>
2023-04-17 21:10:34 +0200

    Christopher changes

--- Lollipop_plot_new.R
+++ Lollipop_plot_new.R
@@ -7,6 +7,7 @@ library(ggExtra)
 library(hrbrthemes)
 library(grid)
 library(cowplot)
+library(plyr)
 
 #Load and clean Data 
 DatenEmma <- read_excel("Desktop/Medien_Vgl_Dicken-Gewichte_Pressure.xlsx", 
@@ -21,15 +22,16 @@ DatenEmma <- read_excel("Desktop/Medien_Vgl_Dicken-Gewichte_Pressure.xlsx",
 colour_palette <- c('#66c2a5' , '#fc8d62' , '#8da0cb' , '#e78ac3')
 
 data_lollipop <- DatenEmma %>% 
-  select(Pressure_max_mmHg, Rupture, Medium) %>% 
+  select(Pressure_max_mmHg, Rupture, Medium) %>%
   drop_na(Pressure_max_mmHg) %>%
   # mutate(ID = c(1:24))
   mutate(Medium=factor(Medium, levels=c('333','454510','5050'))) %>% 
-  mutate(Rupture=factor(Rupture, levels= (c("0","1"))))
-data_lollippop_mean <- data_lollipop %>% group_by(Medium, Rupture) %>% 
-  summarize(mean_preassure = mean(Pressure_max_mmHg)) %>% 
-  mutate(Medium=factor(Medium, levels=c('1','2','3'))) %>% 
-  mutate(Medium=as.numeric(Medium))
+  mutate(Rupture=factor(Rupture, levels = c('0','1')))
+
+data_lollippop_mean <- data_lollipop %>%
+  filter(Rupture == "1")
+data_lollippop_mean <- aggregate(data_lollippop_mean$Pressure_max_mmHg, list(data_lollippop_mean$Medium), mean)
+
 
 
 # data_lollipop <-  droplevels(data_lollipop[!data_lollipop$Medium == '5050',])
@@ -41,12 +43,14 @@ p_lolli <- data_lollipop %>%
   geom_jitter(aes(shape = Rupture , color = Medium) ,  size=3 , stroke = 1, width = 0.3
               
               ) +
-  stat_summary(data = data_lollippop_mean, fun = mean, geom = "errorbarh", 
-               aes(y = Medium, xmin = mean_preassure, xmax = mean_preassure), 
-               color = "red", size = 1.5)+
-  # geom_hline(data = data_lollippop_mean, )
+  # stat_summary(data = data_lollippop_mean, fun = mean, geom = "errorbarh",
+  #              aes(y = Medium, xmin = ..y.., xmax = ..y..),
+  #              color = "red", size = 1.5)+
+  geom_segment(aes(x = 0.6, y = data_lollippop_mean$x[1], xend = 1.4, yend = data_lollippop_mean$x[1], color = levels(Medium)[1]))+
+  geom_segment(aes(x = 1.6, y = data_lollippop_mean$x[2], xend = 2.4, yend = data_lollippop_mean$x[2], color = levels(Medium)[2]))+
+  geom_segment(aes(x = 2.6, y = data_lollippop_mean$x[3], xend = 3.4, yend = data_lollippop_mean$x[3], color = levels(Medium)[3]))+
   scale_shape_manual(values = c(20, 4) , labels  = c("No" , "Yes")) +
-  scale_color_manual(values = colour_palette , labels=c("3|3|3" , "45|45|10" ))+
+  scale_color_manual(values = colour_palette , labels=c("3|3|3" , "45|45|10"  , "50|50"))+
   scale_y_continuous(limits = c(750,1500) , breaks=c(600 , 800 , 1000,1200, 1400) ,
                      expand = c(0,0))+
   scale_x_discrete(breaks = NULL)+
--- plot_grid_strain.R
+++ plot_grid_strain.R
@@ -12,36 +12,61 @@ library(cowplot)
 # pal1 <- choose_palette()
 # pal2 <- choose_palette()
 
+
 #load custom colour scheme, and make custom colour palette
-colours <- read_excel("C:/Users/Asus/Documents/Doktorarbeit II/Tabellen/HexCodes_zugversuche.xlsx",
-                      col_names = FALSE)
-colour_palette <- colours$`...2`[c(1,12,15)]
+# colours <- read_excel("Desktop/HexCodes_zugversuche.xlsx",
+#                       col_types = c('text','skip','text'),
+#                        col_names = c('probe','groupl')) %>% 
+#   mutate(probe = paste0('Probe ',probe))
+# 
+
+# colour_palette <- colours$`...2`[c(1,12,15)]
 colour_brewer_set2 <- c('#66c2a5',
                         '#fc8d62',
                         '#8da0cb',
                         '#e78ac3')
+
+
+# retrieve the thicknesses of CelluloseMediumVgl_2 WDH.xlsx: 
+thickness_CelluloseMediumVgl_2_1  <- read_excel("Desktop/CelluloseMediumVglWdh2.1.xlsx", 
+                                                sheet = "Ergebnisse", range = "A3:L29", 
+                                                col_names = c('probe','dicke','groupl'),
+                                                col_types = c("text", 
+                                                                                 "skip", "skip", "skip", "skip", "skip", 
+                                                                                 "skip", "skip", "numeric","skip", "skip","text")) %>% 
+  drop_na()
+
 ##compile data from CelluloseMediumVgl_2 WDH.xlsx into one workable file.
 #use tab names of experimental excel sheet to iterate.
-zugversuche_daten_sheets <- excel_sheets("C:/Users/Asus/Documents/Doktorarbeit II/Tabellen/CelluloseMediumVglWdh2.1.xlsx") 
+zugversuche_daten_sheets <- excel_sheets("Desktop/CelluloseMediumVglWdh2.1.xlsx") 
 
-#read all experiment names from sheet 2 of experiment output file, 
-# maximum limit of experiments per file set to 998
-# omit all unused entries.
-exp_type <- read_excel("C:/Users/Asus/Documents/Doktorarbeit II/Tabellen/CelluloseMediumVglWdh2.1.xlsx",
+
+# #read all experiment names from sheet 2 of experiment output file, 
+# # maximum limit of experiments per file set to 998
+# # omit all unused entries.
+exp_type <- read_excel("Desktop/CelluloseMediumVglWdh2.1.xlsx",
                        sheet = 2,
                        range = 'A2:A1000',
-                       col_names = 'probe') %>% 
+                       col_names = 'probe') %>%
   na.omit()
 
+# groupl_type <- read_excel("Desktop/CelluloseMediumVglWdh2.1.xlsx",
+#                           sheet = 2,
+#                           range = 'L2:L1000',
+#                           col_names = 'probe') %>% 
+#   na.omit()
+
+
+
 #initiate data frame to be filled in for loop
-zugversuche_datenFull <- data.frame(row.names = c("Dehnung", "Standardkraft"))
+zugversuche_datenFull <- data.frame(row.names = c("Dehnung", "Standardkraft", "probe","dicke","groupl"))
 
 #iterate along the tab names to extract relevant experimental data,
 # start at i=4, as first data is stored from sheet 4.
 for (i in 4:length(zugversuche_daten_sheets)){
   # for (i in 4){
   #make an intermediate variable x that stores all data points within one experiment
-  x <- read_excel("C:/Users/Asus/Documents/Doktorarbeit II/Tabellen/CelluloseMediumVglWdh2.1.xlsx",
+  x <- read_excel("Desktop/CelluloseMediumVglWdh2.1.xlsx",
                   sheet = i,
                   skip = 4
                   ,col_types = c("numeric" , "numeric")
@@ -52,36 +77,56 @@ for (i in 4:length(zugversuche_daten_sheets)){
   x <- x %>% filter(
     # Standardkraft<=(0.8 * max(x$Standardkraft)) &
     # x <- x %>% filter(Standardkraft >= 1.389771            
-    Dehnung <= (Dehnung[which.max(Standardkraft)] + 0.8)
+    Dehnung <= (Dehnung[which.max(Standardkraft)] + 0.5)
   )
   #add $probe with experiment name and $group with experimental group for colour code
   x$probe <- exp_type$probe[i-3]
-  x$groupl <- colours$...3[i-3]
+  x <- x%>% 
+    left_join(thickness_CelluloseMediumVgl_2_1) %>% 
+    na.omit()
   
   #concatenate into full experimental dataframe
   zugversuche_datenFull <- rbind(zugversuche_datenFull,x)
 }
 rm(x) #delete intermediate variable
 
+
 #reorder columns such that probe is the first column
-zugversuche_datenFull2 <- zugversuche_datenFull[,c(3,1,2,4)]
+zugversuche_datenFull2 <- zugversuche_datenFull[,c(3,1,2,4,5)]
 
 ##turn probe into factor 
+# zugversuche_datenFull2 <- zugversuche_datenFull2 %>% 
+#   left_join(thickness_CelluloseMediumVgl_2_1)
 zugversuche_datenFull2$probe <- as.factor(zugversuche_datenFull$probe)
 zugversuche_datenFull2$probe <- factor(zugversuche_datenFull$probe,
                                        levels = unique(zugversuche_datenFull$probe))
 
+# zugversuche_datenFull2 <- zugversuche_datenFull2 %>% 
+#   left_join(thickness_CelluloseMediumVgl_2_1)
+
+zugversuche_datenFull2$Standardkraft_norm <- zugversuche_datenFull2$Standardkraft / zugversuche_datenFull2$dicke
+
 #load earlier dataset "Zugversuche 1: with c100 medium"
-zugversuche1_daten <- read_excel("Doktorarbeit II/Tabellen/Zugversuche_Neu2.xlsx", 
-                                                                sheet = "C100MediumVglWdh1")%>% 
-  mutate(large_group = "group 4") %>% 
-  rename(Probe, "probe" = "Probe") %>% 
-  rename(`Dehnung [%]`, "Dehnung" = "Dehnung [%]") %>% 
-  rename(`Standardkraft [N]`, "Standardkraft" = "Standardkraft [N]") %>% 
-  rename(large_group, "groupl" = "large_group")
-zugversuche1_daten <- zugversuche1_daten[,c(3,1,2,4)]
-zugversuche1_daten$probe2 <- zugversuche1_daten$probe
-zugversuche1_daten$probe <-  as.character(zugversuche1_daten$probe)
+zugversuche1_daten <- read_excel("/Users/ChrisHemingway/Desktop/Zugversuche_Neu2.xlsx",
+                                                                sheet = "C100MediumVglWdh1", col_types = c("numeric", "numeric", "numeric", 
+                                                                                                           "skip", "skip"))%>%
+  mutate(groupl = "group 4")
+colnames(zugversuche1_daten) = c('Dehnung', 'Standardkraft', 'probe' , 'groupl')
+
+zugversuche2_daten <- data.frame(row.names = c("Dehnung", "Standardkraft", "probe","groupl"))
+for (i in 1:length(unique(zugversuche1_daten$probe))){
+  x <- subset(zugversuche1_daten, zugversuche1_daten$probe == unique(zugversuche1_daten$probe)[i]) %>% 
+    filter(Dehnung <= (Dehnung[which.max(Standardkraft)] + 0.5))
+  zugversuche2_daten <- rbind(zugversuche2_daten,x)
+}
+rm(x)
+
+
+zugversuche2_daten <- zugversuche2_daten[,c(3,1,2,4)]
+
+# zugversuche1_daten$probe2 <- zugversuche1_daten$probe
+zugversuche2_daten$probe <-  as.character(zugversuche2_daten$probe)
+
 #rename values of zugversuche1_daten$probe
 #counter_variable <- c(unique(zugversuche1_daten$probe2))
 #for (i in 13:21) {
@@ -92,19 +137,34 @@ zugversuche1_daten$probe <-  as.character(zugversuche1_daten$probe)
      # zugversuche1_daten$probe)
   #)
 #}
-zugversuche1_daten <- zugversuche1_daten[,1:4]
+# zugversuche1_daten <- zugversuche1_daten[,1:4]
+zugversuche1_daten_dicke <- read_excel("/Users/ChrisHemingway/Desktop/Zugversuche_Neu2.xlsx",
+                                                             sheet = "C100MediumVglWdh1", col_types = c("skip", "skip", "skip", 
+                                                                                                        "numeric", "numeric")) %>% 
+  drop_na()
+zugversuche1_daten_dicke$probe <- as.character(zugversuche1_daten_dicke$probe)
 
-zugversuche_datenFull3 <- rbind(zugversuche1_daten , zugversuche_datenFull2)
+zugversuche2_daten <- zugversuche2_daten %>% 
+  left_join(zugversuche1_daten_dicke)
+
+zugversuche2_daten$Standardkraft_norm <- zugversuche2_daten$Standardkraft / zugversuche2_daten$dicke
+
+
+zugversuche_datenFull3 <- rbind(zugversuche_datenFull2,zugversuche2_daten) %>% 
+  drop_na()
+
+
+# zugversuche_datenFull3 <-  zugversuche_datenFull2
 
 # mypal2 <- colorRampPalette(brewer.pal(6, "Greens"))
 # mypal3 <- colorRampPalette(brewer.pal(6, "Purples"))
 
 plot_legend <- zugversuche_datenFull3 %>%
-  ggplot( aes(x=`Dehnung`, y=`Standardkraft`, group = probe
+  ggplot( aes(x=`Dehnung`, y=Standardkraft_norm, group = probe
               , colour = groupl
   ))+
   geom_line()+
-  scale_colour_manual(values = colour_brewer_set2, labels=c("45|45|10", "1/3|1/3|1/3","50|50","c-100"))+
+  scale_colour_manual(values = colour_brewer_set2, labels=c("N-10", "N-30","N-50","C-100"))+
   theme_minimal_grid()+
   theme()+
   # geom_label_repel(aes(label = group),
@@ -118,14 +178,16 @@ for (i in 1:length(unique(zugversuche_datenFull3$groupl))){
     filter(groupl==paste0('group ',i))
   var_p <- paste0('p_zugversuch_',i)
   plot <- daten_temp%>%
-    ggplot( aes(x=`Dehnung`, y=`Standardkraft`, group = probe
+    ggplot( aes(x=`Dehnung`, y=`Standardkraft_norm`, group = probe
                 , colour = groupl
     ))+
     geom_line()+
     scale_colour_manual(values = colour_brewer_set2[i],
-                      labels = c("45|45|10", "1/3|1/3|1/3","50|50","c-100"))+
+                      labels = c("N-10", "N-30","N-50","C-100"))+
     xlim(0,30)+
     ylim(0,3)+
+    ylab('normalised force')+
+    xlab('strain')+
     theme_minimal_grid()+
     theme(
       legend.position="none"
@@ -160,15 +222,15 @@ labs_groups <- c('45|45|10','1/3|1/3|1/3','50|50','C-100')
 
 pg2 <- plot_grid(plotlist = 
                    plotGrid
-                 ,
-                 labels = labs_groups,
-                 label_size = 12
+                 # ,
+                 # labels = labs_groups,
+                 # label_size = 12
 )
 pg2 <- plot_grid(pg2, legend, ncol = 2, rel_widths = c(9,2))
 
 pg2
-ggsave(path = "C:/Users/Asus/Documents/Doktorarbeit II/R_Coding/Github/EmmR", filename = "cellulose_medium_vergleich_asTiles.png", plot = pg2, device = "png" , dpi = 300 ,
+ggsave(path = "/Users/ChrisHemingway/Desktop", filename = "cellulose_medium_vergleich_asTiles.png", plot = pg2, device = "png" , dpi = 300 ,
        units = "cm" , width = 16.8 , height = 11)
-
-ggsave(path = "C:/Users/Asus/Documents/Doktorarbeit II/R_Coding/Github/EmmR",filename = "cellulose_medium_vergleich_ineinem.png", plot = plot_legend, device = "png" , dpi = 300 ,
+# 
+ggsave(path = "/Users/ChrisHemingway/Desktop",filename = "cellulose_medium_vergleich_ineinem.png", plot = plot_legend, device = "png" , dpi = 300 ,
        units = "cm" , width = 16.8 , height = 11)
